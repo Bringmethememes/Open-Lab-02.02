bd5c7770625fbeb7551e4d907f1f8d272b255eb7
Bringmethememes <andrejzidek112@gmail.com>
2020-10-05 13:15:47 +0200

    Som retardovany robil som na tom 3 hodiny

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 
+
 namespace Open_Lab_02._02
 {
     public class Formatter
     {
         public void Format(double[] numbers)
         {
-            throw new NotImplementedException();
+            int k;
+
+            for (k = 0; k < numbers.Length; k++)
+            {
+
+                double Cislo = numbers[k];
+                if (k == 0)
+                {
+                    Console.Write(Math.Round(Cislo, 2).ToString().Replace(",", "."));
+                }
+                else
+                {
+                    Console.Write(" " + Math.Round(Cislo, 2).ToString().Replace(",", "."));
+                }
+               
+                
+                
+            }
+            
+             
+
+
         }
+        
+
+
+
+
     }
+
+
+
+
+    
 }
+
+
+
+
+
+
+      
